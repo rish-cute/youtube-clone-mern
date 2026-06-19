962e1a7081cad16eee1305a9a0450cd9920b3160
rish-cute <rishikareddy1010@gmail.com>
2026-06-19 20:46:53 +0530

    Configure MongoDB Atlas connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
@@ -8,7 +16,7 @@ app.get("/", (req, res) => {
   });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
