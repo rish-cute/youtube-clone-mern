511c0591a68584bd0b60e415b3828d23baf7c970
rish-cute <rishikareddy1010@gmail.com>
2026-06-21 11:54:57 +0530

    Configure Tailwind CSS v4

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,11 @@
 function Home() {
-  return <>Home Page</>;
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-red-600">
+        Tailwind Working
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
